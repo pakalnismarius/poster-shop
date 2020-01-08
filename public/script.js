96b3b44c5770e7c19f27ad95fad10807746e896f
marius <pakalnis.marius@gmail.com>
2020-01-08 08:46:24 +0200

    Adding basic cart functionality

--- public/script.js
+++ public/script.js
@@ -1,1 +1,16 @@
-console.log("It works!");
+new Vue ({
+  el: "#app",
+  data: {
+    total: 0,
+    products: [
+      { title: "Product 1" },
+      { title: "Product 2" },
+      { title: "Product 3" }
+    ]
+  },
+  methods: {
+    addToCart: function() {
+      this.total += 9.99;
+    }
+  }
+});
