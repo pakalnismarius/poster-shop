56ea84fc32188a6854263ec566273f5a09ef5a6a
marius <pakalnis.marius@gmail.com>
2020-01-20 17:32:00 +0200

    showing search result

--- public/script.js
+++ public/script.js
@@ -4,7 +4,8 @@ new Vue ({
     total: 0,
     products: [],
     cart: [],
-    search: ""
+    search: "",
+    lastSearch: ""
   },
   methods: {
     addToCart: function(product) {
@@ -42,6 +43,7 @@ new Vue ({
       this.$http.get(path)
         .then(function(response) {
           this.products = response.body;
+          this.lastSearch = this.search;
         });
     }
   },
